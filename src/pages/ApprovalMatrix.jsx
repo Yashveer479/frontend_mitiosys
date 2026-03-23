94ef3d1c373c0fc12313d05ba71546ceaf3a386d
Yashveer479 <ys083336@gmail.com>
2026-03-23 11:02:52 +0530

    commite this also

--- src/pages/ApprovalMatrix.jsx
+++ src/pages/ApprovalMatrix.jsx
@@ -454,7 +454,7 @@ const ApprovalMatrix = () => {
                         <div className="p-4 sm:p-6 bg-slate-50/50">
                             {selectedRequest.approvalLogs && selectedRequest.approvalLogs.length > 0 ? (
                                 <div className="space-y-4 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
-                                    {selectedRequest.approvalLogs.map((log) => (
+                                    {[...selectedRequest.approvalLogs].sort((a, b) => a.level - b.level).map((log) => (
                                         <div key={log.id} className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
                                             <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-100 text-slate-500 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2 z-10 font-bold text-xs uppercase">
                                                 L{log.level}
